c92776db1bf183fcf53723f33a00abf46cd1afb9
v3b6a7t <v3b6a7t@webbart.eu>
2020-05-15 21:38:48 +0200

    Reorganization of the application structure

---
+++ api/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bookRouter from './routers/bookRouter';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use('/api', bookRouter);
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.set('useFindAndModify', false);
+
+mongoose.connect('mongodb://localhost:27017/bookAPI');
+const db = mongoose.connection;
+
+db.on('error', console.error);
+db.on('open', () => {
+    app.listen(app.get('port'), () => {
+        console.log(`Server is litening on port ${app.get('port')}!`);
+    })
+});
+
+
+
+
