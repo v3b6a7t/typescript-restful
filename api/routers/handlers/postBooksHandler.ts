33d25a8d5e2ae8b3fb7111470c7b249b9bceb546
v3b6a7t <v3b6a7t@webbart.eu>
2020-05-18 10:22:55 +0200

    Generalization of the router

--- api/routers/handlers/postBooksHandler.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import Book from '../../models/bookModel';
-
-export default (req: Request, res: Response): void => {
-    const book = new Book(req.body);
-    book.save();
-    res.status(201).json(book);
-}
--- api/routers/support/getQueryProcessed.ts
+++ api/routers/support/getQueryProcessed.ts
@@ -6,7 +6,7 @@ import { Request } from 'express';
  * @param request 
  * @param model 
  */
-function getQueryProcessed<T extends Model<Document, {}>>(request: Request, model: T): object {
+function getQueryProcessed<T extends Model<Document>>(request: Request, model: T): object {
     return Object.entries(request.query).reduce((succ, [key, val]) => {
         if (key in model.schema.paths) return Object.assign(succ, { [key]: val });
         return succ;
