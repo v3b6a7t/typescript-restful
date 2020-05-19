abe273f49982bc27386c7efc5321d4e5bcd8d40b
v3b6a7t <v3b6a7t@webbart.eu>
2020-05-19 17:50:39 +0200

    GET, POST, PUT and PATCH methods and middleware have been added to the router

--- api/routers/index.ts
+++ api/routers/index.ts
@@ -1,58 +1,75 @@
-import { Router, NextFunction } from 'express';
+import { Router, NextFunction  } from 'express';
 import { Request, Response } from 'express';
 import { Model, Document } from 'mongoose';
 import getQuery from './support/getQuery';
+import getEmpty from './support/getEmpty';
 
-
+// INTERFACE
 export interface ExtendedRequest extends Request {
     docFoundById?: Document;
+    docProcessed?: Document;
 }
 
-
+// GENERIC ROUTER
 export default <T extends Model<Document>>(model: T): Router => {
     const router = Router();
 
+    // ROUTE with search
     router.route('/')
-        .post((req: ExtendedRequest, res: Response): void => {
-            const instance = new model(req.body);
-            instance.save();
-            res.status(201).json(instance);
-        })
-        .get((req: ExtendedRequest, res: Response): void => {
+        .get((req: Request, res: Response): void => {
             const query = getQuery<typeof req.query, typeof model>(req.query, model);
             model.find(query)
-                .sort({ title: 1 })
                 .exec()
                 .then(docs => res.status(200).json(docs))
                 .catch(err => res.status(400).json(err))
+        })
+        .post((req: Request, res: Response): void => {
+            const instance = new model(req.body);
+            instance.save()
+                .then(() => res.status(201).json(instance))
+                .catch(err => res.status(404).json(err));
         });
 
+    // USE MIDDLEWARE by id
     router.use('/:id', (req: ExtendedRequest, res: Response, next: NextFunction) => {
         model.findById(req.params.id)
             .exec()
             .then(doc => {
-                if(doc instanceof model) {
+                if (doc instanceof model) {
                     req.docFoundById = doc;
-                    res.status(200).json(doc)
-                    next()
+                    next();
                 }
+                else res.sendStatus(404);
             })
             .catch(err => res.status(400).json(err))
-    })
+    });
+    router.use('/:id', (req: ExtendedRequest, res: Response, next: NextFunction) => {
+        if(['PUT', 'PATCH'].includes(req.method)) {
+            const body = getQuery<typeof req.body, typeof model>(req.body, model);
+            const empty = (req.method === 'PUT')? getEmpty<typeof body, typeof model>(body, model) : {};
+            req.docProcessed = Object.assign(req.docFoundById, body, empty);
+        }
+        next();
+    });
 
+    // ROUTE by id 
     router.route('/:id')
         .get((req: ExtendedRequest, res: Response): void => {
-            model.findById(req.params.id)
-                .exec()
-                .then(doc => res.status(200).json(doc))
-                .catch(err => res.status(400).json(err))
+            res.status(200).json(req.docFoundById);
         })
-        .put((req: ExtendedRequest, res: Response) => {
-            const body = getQuery<typeof req.body, typeof model>(req.body, model);
-            model.findByIdAndUpdate(req.params.id, { $set: body })
-                .exec()
-                .then(doc => res.status(201).json(doc))
-                .catch(err => res.status(400).json(err))
+        .put((req: ExtendedRequest, res: Response): void => {
+            if(req.docProcessed) {
+                req.docProcessed.save()
+                    .then(() => res.status(201).json(req.docProcessed))
+                    .catch(err => res.status(405).json(err));
+            }
+        })
+        .patch((req: ExtendedRequest, res: Response): void => {
+            if (req.docProcessed) {
+                req.docProcessed.save()
+                    .then(() => res.status(201).json(req.docProcessed))
+                    .catch(err => res.status(405).json(err));
+            }
         });
     return router;
 };
--- api/routers/support/getEmpty.ts
+++ api/routers/support/getEmpty.ts
@@ -3,8 +3,8 @@ import { Model, Document } from 'mongoose';
 const forbidden: string[] = ['_id', '__v'];
 
 export default function <D extends object, M extends Model<Document>>(data: D, model: M): object {
-    return Object.entries(data).reduce((succ, [key, val]) => {
-        if (!forbidden.includes(key) && (key in model.schema.paths)) return Object.assign(succ, { [key]: val });
+    return Object.keys(model.schema.paths).reduce((succ, key) => {
+        if (!forbidden.includes(key) && !(key in data)) return Object.assign(succ, { [key]: undefined });
         return succ;
     }, {})
 }
