98f66898df3ddf1b613bfe55ae14da9da8eea499
v3b6a7t <v3b6a7t@webbart.eu>
2020-07-27 11:23:11 +0200

    Fixed problems indicated by ESLint (#8)

--- api/models/validator/stringValidator.ts
+++ api/models/validator/stringValidator.ts
@@ -1,10 +1,10 @@
 export interface StringValidatorInterface {
-    0: (val: string) => void;
-    1: string;
+    validator: (val: string) => boolean;
+    message: string;
 }
 
 // Change the 'any' type to the best for the validator
-export type StringValidatorType = any & StringValidatorInterface;
+export type StringValidatorType = StringValidatorInterface;
 
 export interface StringValidatorOptionsInterface {
     min?: number;
@@ -15,16 +15,16 @@ export interface StringValidatorOptionsInterface {
 
 function stringValidator(options: StringValidatorOptionsInterface): StringValidatorType {
     const { min = 3, max = 128, match = /^(.*)$/i, info = '' } = options;
-    const getErrorInfo = (errorInfo: string) => info ? errorInfo.concat('. (', info, ')') : errorInfo;
+    const getErrorInfo = (errorInfo: string): string => info ? errorInfo.concat('. (', info, ')') : errorInfo;
 
-    return ([
-        (val: string) => {
+    return ({
+        validator: (val: string): boolean => {
             const testVal = val.trim();
             const testLength = testVal.length;
             return (match.test(testVal) && testLength >= min && testLength <= max);
         },
-        getErrorInfo(`Value of '{PATH}' must be between ${min} and ${max} characters long`)
-    ]);
+        message: getErrorInfo(`Value of '{PATH}' must be between ${min} and ${max} characters long`)
+    });
 }
 
 export default stringValidator;
